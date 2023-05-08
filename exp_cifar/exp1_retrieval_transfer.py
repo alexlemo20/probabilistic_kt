2cbe17c7151f3dedec5efc8e34b8ccbde6f913b9
root <alexlemo@auth.csd.gr>
2023-05-08 07:41:28 +0000

    Added pkt conataining the methods and related modifications for cifar

--- exp_cifar/exp1_retrieval_transfer.py
+++ exp_cifar/exp1_retrieval_transfer.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("/content/probabilistic_kt")
 from nn.nn_utils import load_model, save_model
 from nn.distillation import unsupervised_distillation
 from nn.pkt import knowledge_transfer
@@ -13,18 +15,28 @@ def perform_transfer_knowledge(net, donor_net, transfer_loader, output_path, tra
     # Move the models into GPU
     net.cuda()
     donor_net.cuda()
+    typical_mask = 1
+    atypical_mask = 0
+    typical_target = 2
+    atypical_target = 1
+    atypical_proportion = 0.2
 
+    typical_step = (typical_target - typical_mask) / iters[0]
+    atypical_step = (atypical_target - atypical_mask) / iters[0]
+    
     # Perform the transfer
     W = None
     for lr, iters in zip(learning_rates, iters):
-        if transfer_method == 'hint':
-            W = unsupervised_hint_transfer(net, donor_net, transfer_loader, epochs=iters, lr=lr, W=W)
-        elif transfer_method == 'hint_optimized':
-            W = unsupervised_hint_transfer_optimized(net, donor_net, transfer_loader, epochs=iters, lr=lr, W=W)
-        elif transfer_method == 'distill':
-            unsupervised_distillation(net, donor_net, transfer_loader, epochs=iters, lr=lr, T=distill_temp)
-        elif transfer_method == 'pkt':
-            knowledge_transfer(net, donor_net, transfer_loader, epochs=iters, lr=lr)
+        #if transfer_method == 'hint':
+        #    W = unsupervised_hint_transfer(net, donor_net, transfer_loader, epochs=iters, lr=lr, W=W)
+        #elif transfer_method == 'hint_optimized':
+        #    W = unsupervised_hint_transfer_optimized(net, donor_net, transfer_loader, epochs=iters, lr=lr, W=W)
+        #elif transfer_method == 'distill':
+        #    unsupervised_distillation(net, donor_net, transfer_loader, epochs=iters, lr=lr, T=distill_temp)
+        if transfer_method == 'pkt':
+            knowledge_transfer(net, donor_net, transfer_loader, epochs=iters, lr=lr, typical_mask=typical_mask, typical_step=typical_step, atypical_mask=atypical_mask, atypical_step=atypical_step, atypical_proportion=atypical_proportion)
+            #knowledge_transfer(net, donor_net, transfer_loader, epochs=iters, lr=lr)
+
         else:
             assert False
 
@@ -36,35 +48,35 @@ def evaluate_kt_methods(net_creator, donor_creator, donor_path, transfer_loader,
                         donor_name='very_small_cifar10', net_name='tiny_cifar', transfer_name='cifar10',
                         iters=50, init_model_path=None):
     # Method 1: HINT transfer
-    net = net_creator()
-    if init_model_path is not None:
-        load_model(net, init_model_path)
+    #net = net_creator()
+    #if init_model_path is not None:
+    #    load_model(net, init_model_path)
 
-    donor_net = donor_creator()
-    load_model(donor_net, donor_path)
+    #donor_net = donor_creator()
+    #load_model(donor_net, donor_path)
 
-    train_loader, test_loader, train_loader_raw = transfer_loader(batch_size=batch_size)
-    output_path = 'models/' + net_name + '_' + donor_name + '_hint_' + transfer_name + '.model'
-    results_path = 'results/' + net_name + '_' + donor_name + '_hint_' + '_' + transfer_name + '.pickle'
+    #train_loader, test_loader, train_loader_raw = transfer_loader(batch_size=batch_size)
+    #output_path = 'models/' + net_name + '_' + donor_name + '_hint_' + transfer_name + '.model'
+    #results_path = 'results/' + net_name + '_' + donor_name + '_hint_' + '_' + transfer_name + '.pickle'
 
-    perform_transfer_knowledge(net, donor_net, transfer_loader=train_loader, transfer_method='hint',
-                               output_path=output_path, iters=[iters], learning_rates=[0.0001])
-    evaluate_model_retrieval(net=Cifar_Tiny(num_classes=10), path=output_path, result_path=results_path)
+    #perform_transfer_knowledge(net, donor_net, transfer_loader=train_loader, transfer_method='hint',
+    #                           output_path=output_path, iters=[iters], learning_rates=[0.0001])
+    #evaluate_model_retrieval(net=Cifar_Tiny(num_classes=10), path=output_path, result_path=results_path)
 
     # Method 2: Distillation transfer
-    net = net_creator()
-    if init_model_path is not None:
-        load_model(net, init_model_path)
+    #net = net_creator()
+    #if init_model_path is not None:
+    #    load_model(net, init_model_path)
 
-    donor_net = donor_creator()
-    load_model(donor_net, donor_path)
+    #donor_net = donor_creator()
+    #load_model(donor_net, donor_path)
 
-    train_loader, test_loader, train_loader_raw = transfer_loader(batch_size=batch_size)
-    output_path = 'models/' + net_name + '_' + donor_name + '_distill_' + transfer_name + '.model'
-    results_path = 'results/' + net_name + '_' + donor_name + '_distill_' + transfer_name + '.pickle'
-    perform_transfer_knowledge(net, donor_net, transfer_loader=train_loader, transfer_method='distill',
-                               output_path=output_path, iters=[iters], learning_rates=[0.0001])
-    evaluate_model_retrieval(net=Cifar_Tiny(num_classes=10), path=output_path, result_path=results_path)
+    #train_loader, test_loader, train_loader_raw = transfer_loader(batch_size=batch_size)
+    #output_path = 'models/' + net_name + '_' + donor_name + '_distill_' + transfer_name + '.model'
+    #results_path = 'results/' + net_name + '_' + donor_name + '_distill_' + transfer_name + '.pickle'
+    #perform_transfer_knowledge(net, donor_net, transfer_loader=train_loader, transfer_method='distill',
+    #                           output_path=output_path, iters=[iters], learning_rates=[0.0001])
+    #evaluate_model_retrieval(net=Cifar_Tiny(num_classes=10), path=output_path, result_path=results_path)
 
     # Method 3: PKT transfer
     net = net_creator()
@@ -82,19 +94,19 @@ def evaluate_kt_methods(net_creator, donor_creator, donor_path, transfer_loader,
 
 
     # Method 4: HINT (optimized) transfer
-    net = net_creator()
-    if init_model_path is not None:
-        load_model(net, init_model_path)
+    #net = net_creator()
+    #if init_model_path is not None:
+    #    load_model(net, init_model_path)
 
-    donor_net = donor_creator()
-    load_model(donor_net, donor_path)
+    #donor_net = donor_creator()
+    #load_model(donor_net, donor_path)
 
-    train_loader, test_loader, train_loader_raw = transfer_loader(batch_size=batch_size)
-    output_path = 'models/' + net_name + '_' + donor_name + '_hint_optimized_' + transfer_name + '.model'
-    results_path = 'results/' + net_name + '_' + donor_name + '_hint_optimized_' + '_' + transfer_name + '.pickle'
-    perform_transfer_knowledge(net, donor_net, transfer_loader=train_loader, transfer_method='hint_optimized',
-                               output_path=output_path, iters=[iters], learning_rates=[0.0001])
-    evaluate_model_retrieval(net=Cifar_Tiny(num_classes=10), path=output_path, result_path=results_path)
+    #train_loader, test_loader, train_loader_raw = transfer_loader(batch_size=batch_size)
+    #output_path = 'models/' + net_name + '_' + donor_name + '_hint_optimized_' + transfer_name + '.model'
+    #results_path = 'results/' + net_name + '_' + donor_name + '_hint_optimized_' + '_' + transfer_name + '.pickle'
+    #perform_transfer_knowledge(net, donor_net, transfer_loader=train_loader, transfer_method='hint_optimized',
+    #                           output_path=output_path, iters=[iters], learning_rates=[0.0001])
+    #evaluate_model_retrieval(net=Cifar_Tiny(num_classes=10), path=output_path, result_path=results_path)
 
 if __name__ == '__main__':
     evaluate_kt_methods(lambda: Cifar_Tiny(10), lambda: ResNet18(num_classes=10), 'models/resnet18_cifar10.model',
--- nn/pkt.py
+++ nn/pkt.py
@@ -3,9 +3,10 @@ from torch.autograd import Variable
 import torch.optim as optim
 from tqdm import tqdm
 import numpy as np
+import matplotlib
+from matplotlib.pylab import plt
 
-
-def knowledge_transfer(net, net_to_distill, transfer_loader, epochs=1, lr=0.0001, supervised_weight=0):
+def knowledge_transfer(net, net_to_distill, transfer_loader, epochs=1, lr=0.0001, typical_mask=1, typical_step=0, atypical_mask=1, atypical_step=0, atypical_proportion=0, supervised_weight=0):
     """
     Performs unsupervised neural network knowledge transfer
     :param net:
@@ -15,14 +16,19 @@ def knowledge_transfer(net, net_to_distill, transfer_loader, epochs=1, lr=0.0001
     :param lr:
     :return:
     """
-
     optimizer = optim.Adam(params=net.parameters(), lr=lr)
+    #typical_losses = list()
+    #atypical_losses = list()
+    #overall_losses = list()
 
+    #step = 1 / epochs
+    #par_step = 0
     for epoch in range(epochs):
 
         net.train()
         net_to_distill.eval()
-
+        #typical_loss = 0
+        #atypical_loss = 0
         train_loss = 0
         counter = 1
         for (inputs, targets) in tqdm(transfer_loader):
@@ -40,7 +46,12 @@ def knowledge_transfer(net, net_to_distill, transfer_loader, epochs=1, lr=0.0001
                 loss = cosine_similarity_loss(outputs_net, output_target) + \
                        supervised_weight * supervised_loss(outputs_net, targets)
             else:
-                loss = cosine_similarity_loss(outputs_net, output_target)
+                #loss = cosine_similarity_loss(outputs_net, output_target)
+                #loss, typ_loss, atyp_loss = cosine_similarity_loss(outputs_net, output_target, targets, typical_mask, atypical_mask, atypical_proportion)
+                loss = cosine_similarity_loss(outputs_net, output_target, targets, typical_mask, atypical_mask, atypical_proportion) #par_step
+                
+            #typical_loss += typ_loss.cpu().data.item()
+            #atypical_loss += atyp_loss.cpu().data.item()
 
             loss.backward()
             optimizer.step()
@@ -48,12 +59,44 @@ def knowledge_transfer(net, net_to_distill, transfer_loader, epochs=1, lr=0.0001
             train_loss += loss.cpu().data.item()
             counter += 1
 
+        typical_mask += typical_step
+        atypical_mask += atypical_step
+        #par_step += step
+
+        #typical_loss /= float(counter)
+        #atypical_loss /= float(counter)
+        #print("Typical loss = ",typical_loss," Atypical loss = ", atypical_loss)
         train_loss = train_loss / float(counter)
         print("\n Epoch = ", epoch, " Loss  = ", train_loss)
 
+        #typical_losses.append(typical_loss)
+        #atypical_losses.append(atypical_loss)
+        #overall_losses.append(train_loss)
+    
+    # Plot losses
+    #plt.plot(range(epochs), typical_losses, label='Typical Loss')
+    #plt.plot(range(epochs), atypical_losses, label='Atypical Loss')
+    #plt.plot(range(epochs), overall_losses, label='Overall Loss')
+    #plt.title('Typical, Atypical and Overall Losses')
+    #plt.xlabel('Epochs')
+    #plt.ylabel('Loss')
+    # Display the plot
+    #plt.legend(loc='best')
+    #plt.show()
+    #plt.savefig('my_plot.png')
 
-def knowledge_transfer_handcrafted(net, transfer_loader, epochs=1, lr=0.0001, supervised_weight=0):
+
+
+def knowledge_transfer_handcrafted(net, transfer_loader, epochs=1, lr=0.0001, supervised_weight=0, typical_mask=1, typical_step=0, atypical_mask=1, atypical_step=0, atypical_proportion=0):
     optimizer = optim.Adam(params=net.parameters(), lr=lr)
+    #typical_mask = 1
+    #atypical_mask = 0
+    #typical_target = 2
+    #atypical_target = 1
+    #atypical_proportion = 0.2
+
+    #typical_step = (typical_target - typical_mask)/epochs
+    #atypical_step = (atypical_target - atypical_mask)/epochs
 
     for epoch in range(epochs):
 
@@ -72,25 +115,31 @@ def knowledge_transfer_handcrafted(net, transfer_loader, epochs=1, lr=0.0001, su
             outputs_net = net.get_features(Variable(inputs))
 
             # Get the loss
-            loss = cosine_similarity_loss(outputs_net, output_target) + \
+            #loss = cosine_similarity_loss(outputs_net, output_target) + \
+            #       supervised_weight * supervised_loss(outputs_net, targets)
+            loss = cosine_similarity_loss(outputs_net, output_target, targets, typical_mask, atypical_mask, atypical_proportion) + \
                    supervised_weight * supervised_loss(outputs_net, targets)
 
             loss.backward()
             optimizer.step()
 
-            train_loss += loss.cpu().data[0]
+            train_loss += loss.cpu().data.item()
             counter += 1
-
+        
+        typical_mask += typical_step
+        atypical_mask += atypical_step
+        
         train_loss = train_loss / float(counter)
         print("\n Epoch = ", epoch, " Loss  = ", train_loss)
 
 
-def cosine_similarity_loss(output_net, target_net, eps=0.0000001):
+def cosine_similarity_loss(output_net, target_net, targets, typical_mask, atypical_mask, atypical_proportion, step=0,  eps=0.0000001):
+    
     # Normalize each vector by its norm
     output_net_norm = torch.sqrt(torch.sum(output_net ** 2, dim=1, keepdim=True))
     output_net = output_net / (output_net_norm + eps)
     output_net[output_net != output_net] = 0
-
+    
     target_net_norm = torch.sqrt(torch.sum(target_net ** 2, dim=1, keepdim=True))
     target_net = target_net / (target_net_norm + eps)
     target_net[target_net != target_net] = 0
@@ -106,11 +155,127 @@ def cosine_similarity_loss(output_net, target_net, eps=0.0000001):
     # Transform them into probabilities
     model_similarity = model_similarity / torch.sum(model_similarity, dim=1, keepdim=True)
     target_similarity = target_similarity / torch.sum(target_similarity, dim=1, keepdim=True)
+    # Calculate the KL-divergence    
+    #loss = torch.mean(target_similarity * torch.log((target_similarity + eps) / (model_similarity + eps)))
+    
+    # choose to work with the student or the teacher model
+    flag = False
+    if flag == True:
+      cosine_similarity = model_similarity #student
+    else :
+      cosine_similarity = target_similarity #teacher
 
-    # Calculate the KL-divergence
-    loss = torch.mean(target_similarity * torch.log((target_similarity + eps) / (model_similarity + eps)))
+    # choose to shape a symmetric matrix
+    #cosine_similarity = cosine_similarity + torch.transpose(cosine_similarity, 0, 1) / 2
+    labels = targets.cpu().numpy()
 
+    intra_similarities = {}
+    inter_similarities = {}
+
+    for i in range(cosine_similarity.shape[0]):
+      for j in range(i, cosine_similarity.shape[1]): #i, if a symmetric matrix 
+        # intra similarity
+        if labels[i] == labels[j]:
+          intra_similarities[str(i) + " " + str(j)] = cosine_similarity[i][j].item()
+          # comment out if not a symmetric matrix
+          #intra_similarities[str(j) + " " + str(i)] = cosine_similarity[j][i].item()
+        else :
+          inter_similarities[str(i) + " " + str(j)] = cosine_similarity[i][j].item()
+          # comment out if not a symmetric matrix
+          #inter_similarities[str(j) + " " + str(i)] = cosine_similarity[j][i].item()
+
+    # sort the intra and inter similarities
+    intra_sorted = dict(sorted(intra_similarities.items(), key = lambda values:values[1]))
+    inter_sorted = dict(sorted(inter_similarities.items(), key = lambda values:values[1], reverse = True))
+    # choose the atypical similarities size
+    atypical_size_intra = round(atypical_proportion * len(intra_similarities))
+    atypical_size_inter = round(atypical_proportion * len(inter_similarities))
+
+    # choose atypical pairs
+    atypical_intra = list(intra_sorted.keys())[:atypical_size_intra]
+    atypical_inter = list(inter_sorted.keys())[:atypical_size_inter]
+    # initialize the matrix which will be used for the involvement of typical and atypical pairs
+    typical_similarities = torch.cuda.FloatTensor(cosine_similarity.shape[0], cosine_similarity.shape[1]).fill_(typical_mask) # typical_mask
+    #typical_loss_matrix = torch.cuda.FloatTensor(cosine_similarity.shape[0], cosine_similarity.shape[1]).fill_(1)
+    #atypical_loss_matrix = torch.cuda.FloatTensor(cosine_similarity.shape[0], cosine_similarity.shape[1]).fill_(0)
+
+
+
+
+    # 1st method
+    atypical = atypical_intra + atypical_inter
+    #   atypical_loss = 0
+    for coordinates in atypical:
+      x, y = coordinates.split(' ')
+      typical_similarities[int(x)][int(y)] = atypical_mask # atypical_mask
+      # comment out if not to shape a symmetric mask matrix
+      #typical_similarities[int(y)][int(x)] = atypical_mask
+      
+      #typical_loss_matrix[int(x)][int(y)] = 0
+      #typical_loss_matrix[int(y)][int(x)] = 0
+      #atypical_loss_matrix[int(x)][int(y)] = 1
+      #atypical_loss_matrix[int(y)][int(x)] = 1
+      
+    #typical_loss =  torch.mean(target_similarity * torch.log((target_similarity + eps) / (model_similarity + eps)) * typical_loss_matrix)
+    #atypical_loss =  torch.mean(target_similarity * torch.log((target_similarity + eps) / (model_similarity + eps)) * atypical_loss_matrix)
+
+
+
+    # 2nd method 
+    #   typical_intra = list(intra_sorted.keys())[-(len(intra_sorted)-atypical_size_intra):]
+    #   typical_inter = list(inter_sorted.keys())[-(len(inter_sorted)-atypical_size_inter):]
+    
+    #mean_intra = torch.mean(torch.tensor(list(intra_sorted.values())))
+    #max_dist_intra = max(abs(list(intra_sorted.values())[0]-mean_intra), abs(list(intra_sorted.values())[-1]-mean_intra)) # first & last element
+    #mean_inter = torch.mean(torch.tensor(list(inter_sorted.values())))
+    #max_dist_inter = max(abs(list(inter_sorted.values())[0]-mean_inter), abs(list(inter_sorted.values())[-1]-mean_inter)) # first & last element
+   
+    #typical_similarities = torch.cuda.FloatTensor(cosine_similarity.shape[0], cosine_similarity.shape[1]).fill_(1)
+
+    #for coordinates in atypical_intra:
+    #  x, y = coordinates.split(' ')
+    #  typical_similarities[int(x)][int(y)] = ((abs(intra_sorted[coordinates] - mean_intra) / max_dis_intra) + 0.5) * atypical_mask
+    #  typical_similarities[int(y)][int(x)] = ((abs(intra_sorted[coordinates] - mean_intra) / max_dis_intra) + 0.5) * atypical_mask
+
+    #for coordinates in atypical_inter:
+    #  x, y = coordinates.split(' ')
+    #  typical_similarities[int(x)][int(y)] = ((abs(inter_sorted[coordinates] - mean_inter) / max_dis_inter)  + 0.5) * atypical_mask
+    #  typical_similarities[int(y)][int(x)] = ((abs(inter_sorted[coordinates] - mean_inter) / max_dis_inter)  + 0.5) * atypical_mask
+
+    #for coordinates in typical_intra:
+    #  x, y = coordinates.split(' ')
+    #  typical_similarities[int(x)][int(y)] = ((abs(intra_sorted[coordinates] - mean_intra) / max_dis_intra) + 0.5) * typical_mask
+    #  typical_similarities[int(y)][int(x)] = ((abs(intra_sorted[coordinates] - mean_intra) / max_dis_intra) + 0.5) * typical_mask
+  
+    #for coordinates in atypical_inter:
+    #  x, y = coordinates.split(' ')
+    #  typical_similarities[int(x)][int(y)] = ((abs(inter_sorted[coordinates] - mean_inter) / max_dis_inter)  + 0.5) * typical_mask
+    #  typical_similarities[int(y)][int(x)] = ((abs(inter_sorted[coordinates] - mean_inter) / max_dis_inter)  + 0.5) * typical_mask
+
+
+
+    # 3rd method
+    #intra
+    #min_intra = intra_sorted.values()[-1]
+    #max_dis_intra = intra_sorted.values()[0] - intra_sorted.values()[-1]
+    #inter
+    #max_inter = inter_sorted.values()[-1]
+    #max_dis_inter = inter_sorted.values()[-1] - inter_sorted.values()[0]
+    #typical_similarities = torch.cuda.FloatTensor(cosine_similarity.shape[0], cosine_similarity.shape[1]).fill_(1)
+    #for coordinates in intra_sorted:
+    #  x, y = coordinates.split(' ')
+    #  typical_similarities[int(x)][int(y)] = (abs(((intra_sorted[coordinates] - min_intra) / max_dis_intra) - step) / 2) + 0.5
+    #  typical_similarities[int(y)][int(x)] = (abs(((intra_sorted[coordinates] - min_intra) / max_dis_intra) - step) / 2) + 0.5
+
+    #for coordinates in inter_sorted:
+    #  x, y = coordinates.split(' ')
+    #  typical_similarities[int(x)][int(y)] = (abs(((max_inter - inter_sorted[coordinates]) / max_dis_inter) - step) / 2) + 0.5
+    #  typical_similarities[int(y)][int(x)] = (abs(((max_inter - inter_sorted[coordinates]) / max_dis_inter) - step) / 2) + 0.5
+
+    
+    loss = torch.mean(target_similarity * torch.log((target_similarity + eps) / (model_similarity + eps)) * typical_similarities)
     return loss
+    #return loss, typical_loss, atypical_loss 
 
 
 def supervised_loss(output_net, targets, eps=0.0000001):
